4a924e9cefecd63765c94aa31123cd2b7a8984fa
Fer Sanchez <a01746138@tec.mx>
2023-08-08 12:17:40 -0600

    Modification
    Signed-off-by: Fer Sanchez <a01746138@tec.mx>

--- GenerateRuns.py
+++ GenerateRuns.py
@@ -2,7 +2,7 @@ from RunAlgorithm import RunAlgorithm
 
 
 lam = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-n_exp = 1000
+n_exp = 31
 
 alg = RunAlgorithm(lam=lam, n_exp=n_exp)
 alg.sms_evaluate()
